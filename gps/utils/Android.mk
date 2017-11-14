b8aff84b6b36ff28fba423933d08ab3174f3468c
sattarvoybek <sattarvoybek>
2017-11-15 01:49:07 +0300

    fix for O

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -44,8 +44,8 @@ LOCAL_COPY_HEADERS:= \
    loc_log.h \
    loc_cfg.h \
    log_util.h \
-   linked_list.h \
-   msg_q.h \
+   linked_list.cpp \
+   msg_q.cpp \
    MsgTask.h \
    LocHeap.h \
    LocThread.h \
@@ -59,7 +59,6 @@ LOCAL_COPY_HEADERS:= \
    loc_misc_utils.h
 
 LOCAL_MODULE := libgps.utils
-LOCAL_CLANG := false
 
 LOCAL_MODULE_TAGS := optional
 
