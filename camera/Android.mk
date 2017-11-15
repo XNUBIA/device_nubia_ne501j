19d4fa83e945dae7a1f50212edfb31ac47fcf4f3
sattarvoybek <sattarvoybek>
2017-11-15 09:08:27 +0300

    fix

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-    CameraWrapper.cpp
+LOCAL_SRC_FILES := CameraWrapper.cpp
+LOCAL_MODULE := camera.NE501J
 
 LOCAL_SHARED_LIBRARIES := \
     libhardware \
