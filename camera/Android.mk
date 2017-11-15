974779644fe77d792ca1684bfc416f5ae5785573
sattarvoybek <sattarvoybek>
2017-11-15 09:30:58 +0300

    fix

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,36 +1,18 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := camera.bacon
 LOCAL_SRC_FILES := CameraWrapper.cpp
 
+LOCAL_C_INCLUDES := \
+    system/media/camera/include
+
+LOCAL_STATIC_LIBRARIES := libbase libarect
 LOCAL_SHARED_LIBRARIES := \
-    libhardware \
-    libbase \
-    libhidltransport \
-    liblog \
-    libcamera_client \
-    libutils \
-    libcutils \
-    libgui \
-    android.hidl.token@1.0-utils
+    libhardware liblog libcamera_client libutils libcutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
-LOCAL_STATIC_LIBRARIES := \
-     libarect
-
-LOCAL_C_INCLUDES += \
-    system/media/camera/include \
-    framework/native/include 
-
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.qcom
-LOCAL_MODULE_TAGS := optional
-
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_CFLAGS := -Werror
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-LOCAL_32_BIT_ONLY := true
-include $(BUILD_SHARED_LIBRARY)
-
-#include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
