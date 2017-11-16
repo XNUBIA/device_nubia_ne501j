93c0d1351d2d79565b2cf7f91e451326e9713f85
sattarvoybek <sattarvoybek>
2017-11-16 10:33:16 +0300

    up

--- NE501J.mk
+++ NE501J.mk
@@ -73,6 +73,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audiod \
     audio.a2dp.default \
     audio_policy.msm8226 \
@@ -104,7 +106,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     camera.msm8226 \
     libshim_camera \
-    libcamera_parameters_shim \
     Camera2
 
 # Connectivity Engine support
