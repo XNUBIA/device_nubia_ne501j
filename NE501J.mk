830a72bf1d3bc3d9ea998b9d4f058b143ac09233
sattarvoybek <sattarvoybek>
2017-12-02 04:41:13 +0300

    nubia: Move Lineage specific overlays to overlay-lineage

--- NE501J.mk
+++ NE501J.mk
@@ -18,7 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/NUBIA/NE501J/NE501J-vendor.mk)
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+#    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
