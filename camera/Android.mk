3a4795fbd02dd1375ae9a82808d1d80c44820563
sattarvoybek <sattarvoybek>
2017-12-02 03:47:10 +0300

    ne501j: camera: update dependencies for O

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,14 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := CameraWrapper.cpp
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
 
 LOCAL_C_INCLUDES := \
-    system/media/camera/include
+    system/media/camera/include \
+    framework/native/include
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libc  libgui libbase
+    android.hidl.token@1.0-utils \
+    libhidltransport \
+    libhardware \
+    liblog \
+    libcamera_client \
+    libutils \
+    libcutils \
+    libc \
+    libgui \
+    libbase
 
+LOCAL_STATIC_LIBRARIES := \
+    libarect
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
